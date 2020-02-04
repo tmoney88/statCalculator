180885df9b4f2911f9f9c4ab9c899de67a4e8dcb
Tanner Gates <tgates4@dmail.dixie.edu>
2020-02-04 08:20:10 -0700

    First commit, created index.html, main.js, style.css and saved images

---
+++ main.js
@@ -0,0 +1,5 @@
+const baseball = document.querySelector('#baseball-selector');
+const basketball = document.querySelector('#basketball-selector');
+const football = document.querySelector('#football-selector');
+
+
