f3cf992f399757ee1ecbe2aed197ad18af60a93b
Tanner Gates <tgates4@dmail.dixie.edu>
2020-02-06 07:31:50 -0700

    Used querySelectorAll and added event listener to js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-const baseball = document.querySelector('#baseball-selector');
-const basketball = document.querySelector('#basketball-selector');
-const football = document.querySelector('#football-selector');
+// const baseball = document.querySelector('#baseball-selector');
+// const basketball = document.querySelector('#basketball-selector');
+// const football = document.querySelector('#football-selector');
 
 
+const selectedSport = document.querySelectorAll('.sport-selector');
+
+function highlighted (e) {
+    this.classList.add('selected');
+}
+
+selectedSport.forEach(sport => sport.addEventListener('click', highlighted));
