4c44e811d74cb757674cf9eb826c14417da13e5a
Tanner Gates <tgates4@dmail.dixie.edu>
2020-02-06 07:57:59 -0700

    Added 3 individual functions to main.js

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-// const baseball = document.querySelector('#baseball-selector');
-// const basketball = document.querySelector('#basketball-selector');
-// const football = document.querySelector('#football-selector');
+const baseball = document.querySelector('#baseball-selector');
+const basketball = document.querySelector('#basketball-selector');
+const football = document.querySelector('#football-selector');
 
 
-const selectedSport = document.querySelectorAll('.sport-selector');
+const allSports = document.querySelectorAll('.sport-selector');
 
-function highlighted (e) {
-    this.classList.add('selected');
+// function highlighted (e) {
+//     this.classList.add('selected');
+// }
+
+// selectedSport.forEach(sport => sport.addEventListener('click', highlighted));
+
+function baseballSelected(e) {
+    baseball.classList.add('selected');
+    basketball.classList.remove('selected');
+    football.classList.remove('selected');
 }
 
-selectedSport.forEach(sport => sport.addEventListener('click', highlighted));
+function basketballSelected(e) {
+    basketball.classList.add('selected');
+    football.classList.remove('selected');
+    baseball.classList.remove('selected');
+}
+
+function footballSelected(e) {
+    football.classList.add('selected');
+    baseball.classList.remove('selected');
+    basketball.classList.remove('selected');
+}
+
+baseball.addEventListener('click', baseballSelected);
+basketball.addEventListener('click', basketballSelected);
+football.addEventListener('click', footballSelected);
